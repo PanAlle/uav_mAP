2e5f8b7b859a2327f56389796dee3a81e0b2159b
Alessandro Panini <ge97dil@mytum.de>
2020-07-10 12:35:49 +0200

    Code is checked The test image has a overlapping of 80%, implemented with SURF algorithm
    added some files as images for testing

--- uav_mAP.py
+++ uav_mAP.py
@@ -18,25 +18,22 @@ def findDimensions(image, homography):
     base_p3[:2] = [0, y]
     base_p4[:2] = [x, y]
 
-    # Initialize va for maxi and min value for
+    # Initialize variables for max and min, in order to define overall image frame
     max_x = None
     max_y = None
     min_x = None
     min_y = None
 
-    # For each one of the points we need to apply the Homography matrix in order to transform into the new space
+    # For each one of the points we need to apply the inverse Homography matrix in order to transform the points of the next image to the base frame
     for pt in [base_p1, base_p2, base_p3, base_p4]:
 
-        #hp = np.matrix(homography, np.float32) * np.matrix(pt, np.float32).T
-        # Transform base image point into the new space
 
+        # Transform next_image edges into the base space
         hp = np.dot(np.array(homography, np.float32), np.array(pt, np.float32).reshape(-1,1))
 
-        # Normalize the coordinate, by defining the correct point the output is a line vector
+        # Normalize the coordinates, by defining the correct point the output is a line vector
         normal_pt = np.array([hp[0, 0] / hp[2, 0], hp[1, 0] / hp[2, 0]], np.float32)
-
-        # normal_pt[] is a line vector will give directly the float value and, even if there is only one value in row zero
-
+        # In order to define a rectangular hull containing the image, keep only min and max values of x,y
         if (max_x == None or normal_pt[0] > max_x):
             max_x = normal_pt[0]
 
@@ -56,50 +53,61 @@ def findDimensions(image, homography):
 
 def features_detection(img, type):
     if type == "ORB":
-        descriptor = cv2.ORB_create(nfeatures=1000)
-        # Other than none we can pass a mask to cover parts of the image that is not needed
-        kp_pt, kp_descriptor = descriptor.detectAndCompute(img, None)
-        return kp_pt, kp_descriptor
+        detected_points = 4000
+        descriptor = cv2.ORB_create(nfeatures=detected_points)
+    elif type == "SURF":
+        descriptor = cv2.xfeatures2d.SURF_create()
+    # Other than none we can pass a mask to cover parts of the image that is not needed
+    kp_pt, kp_descriptor = descriptor.detectAndCompute(img, None)
+    return kp_pt, kp_descriptor
 
 
 def feature_matching(base_img_descriptor, next_img_descriptor):
     # Create a Brute Force matcher object, for ORB is advised to use HAMMING distance
     # crossCheck=True store only cross correlated matches between the two images
 
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    #bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     # Match the descriptors giving as input the two images descriptor
 
-    matches = bf.match(base_img_descriptor, next_img_descriptor)
+    #matches = bf.match(base_img_descriptor, next_img_descriptor)
     # Sort the matches in order of distance
+    bf = cv2.BFMatcher()
+    #give the two best matches for each descriptor
+    matches = bf.knnMatch(base_img_descriptor, next_img_descriptor, k=2)
+    #sorted_matches = sorted(matches, key=lambda m: m.distance)
 
-    sorted_matches = sorted(matches, key=lambda m: m.distance)
-    return sorted_matches
+    good = []
+    for m, n in matches:
+        if m.distance < 0.8 * n.distance:
+            good.append(m)
+    return good
 
 
-def find_homography(kp_pt_img, kp_pt_query, sorted_matches):
-    src_pts = np.float32([kp_pt_img[m.queryIdx].pt for m in sorted_matches[0:40]]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp_pt_query[m.trainIdx].pt for m in sorted_matches[0:40]]).reshape(-1, 1, 2)
+def find_homography(kp_base_img, kp_next_img, sorted_matches):
+    src_pts = np.float32([kp_base_img[m.queryIdx].pt for m in sorted_matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp_next_img[m.trainIdx].pt for m in sorted_matches]).reshape(-1, 1, 2)
     ## find homography matrix and do perspective transform by using of RANSAC
     H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
     if H is None:
         print("No Homography")
     else:
-        #print(H)
+        print(H)
         return H
 
 
 def stitching(base_img, next_img, H):
     # Normalize to maintaing homogeneus coordianate system
     H = H / H[2, 2]
+    #Inverse matrix
     H_inv = linalg.inv(H)
-
+    print("Inverse matrix",H_inv)
+    #Find the rectangular hull containing the next_img in the base_img reference frame
     (min_x, min_y, max_x, max_y) = findDimensions(next_img, H_inv)
 
-    # Adjust max_x and max_y by base img size
+    # Adjust max_x, max_y in order to include also the first image
     max_x = max(max_x, base_img.shape[1])
     max_y = max(max_y, base_img.shape[0])
-
-    #move_h = np.matrix(np.identity(3), np.float32)
+    # Define the move vector for each pixel
     move_h = np.identity(3 , np.float32)
     # Translate the origin of the image in the positive part of the plane, in order to see it
     if (min_x < 0):
@@ -111,44 +119,47 @@ def stitching(base_img, next_img, H):
         max_y += -min_y
 
     # Inverse translation vector, from H space to normal space
-    mod_inv_h = np.dot(move_h,H_inv)
+    mod_inv_h = np.dot(H_inv,move_h)
     # Return the closest integer near a given number
     img_w = int(math.ceil(max_x))
     img_h = int(math.ceil(max_y))
-    print(mod_inv_h)
 
-
-    # Warp the new image given the homography from the old image
-
-    base_img_warp = cv2.warpPerspective(next_img, move_h, (img_w, img_h))
-    next_img_warp = cv2.warpPerspective(base_img, mod_inv_h, (img_w, img_h))
+    # Warp the new image given the homography from the old imagemogra
+    base_img_warp = cv2.warpPerspective(base_img, move_h, (img_w, img_h))
+    next_img_warp = cv2.warpPerspective(next_img, mod_inv_h, (img_w, img_h))
     #next_img_warp = base_img
     enlarged_base_img = np.zeros((img_h, img_w, 3), np.uint8)
 
 
-    # enlarged_base_img[y:y+base_img_rgb.shape[0],x:x+base_img_rgb.shape[1]] = base_img_rgb
-    # enlarged_base_img[:base_img_warp.shape[0],:base_img_warp.shape[1]] = base_img_warp
+    #enlarged_base_img[y:y+base_img_rgb.shape[0],x:x+base_img_rgb.shape[1]] = base_img_rgb
+    #enlarged_base_img[:base_img_warp.shape[0],:base_img_warp.shape[1]] = base_img_warp
 
-    # Create a mask from the warped image for constructing masked composite
+    # Create a mask from the warped image for constructing masked composite (insert black base on next image, covering the first one)
     (ret, data_map) = cv2.threshold(cv2.cvtColor(next_img_warp, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
-
     enlarged_base_img = cv2.add(enlarged_base_img, base_img_warp, mask=np.bitwise_not(data_map),dtype=cv2.CV_8U)
-
-    # Now add the warped image
+    print(enlarged_base_img.shape)
+    print(next_img_warp.shape)
+    # Now add the warped image with 8bit/pixel (0 - 255)
     final_img = cv2.add(enlarged_base_img, next_img_warp, dtype=cv2.CV_8U)
 
-    cv2.imshow("try", cv2.resize(final_img,(640,480)))
+    #cv2.imshow("try", cv2.resize(final_img,(640,480)))
+    cv2.imshow("try", final_img)
+    cv2.imwrite("img_folder/test3_orb3000_07dlim.jpeg", final_img)
     cv2.waitKey()
 
 
 if __name__ == "__main__":
-    base_img = cv2.imread("img_folder/IMG_7104.jpg")
-    next_img = cv2.imread("img_folder/IMG_7105.jpg")
+    base_img = cv2.imread("img_folder/WhatsApp Image 2020-07-08 at 17.08.03.jpeg")
+    next_img = cv2.imread("img_folder/WhatsApp Image 2020-07-08 at 17.08.03(1).jpeg")
+
+    # base_img = cv2.imread("img_folder/IMG_7105.jpg")
+    # next_img = cv2.imread("img_folder/IMG_7106.jpg")
+
     img1_GS = cv2.cvtColor(base_img, cv2.COLOR_BGR2GRAY)
     img2_GS = cv2.cvtColor(next_img, cv2.COLOR_BGR2GRAY)
 
-    kp_base_img, kp_descriptor_base_img = features_detection(img2_GS, "ORB")
-    kp_next_img, kp_descriptor_next_img = features_detection(img1_GS, "ORB")
+    kp_base_img, kp_descriptor_base_img = features_detection(img1_GS, "SURF")
+    kp_next_img, kp_descriptor_next_img = features_detection(img2_GS, "SURF")
 
     sorted_matches = feature_matching(kp_descriptor_base_img, kp_descriptor_next_img)
 
