a0cdfd784f9dff3909d35159b30ccb8db19aaa84
Alessandro Panini <ge97dil@mytum.de>
2020-08-03 14:27:43 +0200

    For elliptical path sine wave for z axis implemented

--- test_path.py
+++ test_path.py
@@ -51,12 +51,16 @@ def gen_elliptical_path(img_path, sample):
     map = cv2.imread(img_path)
     x = []
     y = []
+    z = []
     for theta in range(0, 360, int(360/sample)):
         x.append((int(0.5 * map.shape[1] / 2 * math.cos(math.radians(theta))) + map.shape[1] / 2))
         y.append((int(0.5 * map.shape[0] / 2 * math.sin(math.radians(theta))) + map.shape[0] / 2))
-    pt = np.array((x, y))
-    plt.scatter(x,y)
+        z.append(0.02*math.sin(math.radians(8*theta)))
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z, c='r', marker='o')
     plt.show()
+    pt = np.array((x, y, z))
     return pt
 
 def gen_coll_elliptical_path(img_path, sample, rotation):
@@ -66,24 +70,27 @@ def gen_coll_elliptical_path(img_path, sample, rotation):
     # print(pt)
     for theta in range(0, rotation*360, int(rotation*360/sample)):
         lmb = 1 - theta / (rotation*360)
-        x.append((int(lmb * 0.8 * map.shape[1] / 2 * math.cos(math.radians(theta))) + map.shape[1] / 2))
-        y.append((int(lmb * 0.8 * map.shape[0] / 2 * math.sin(math.radians(theta))) + map.shape[0] / 2))
+        x.append((int(lmb * 0.5 * map.shape[1] / 2 * math.cos(math.radians(theta))) + map.shape[1] / 2))
+        y.append((int(lmb * 0.5 * map.shape[0] / 2 * math.sin(math.radians(theta))) + map.shape[0] / 2))
     pt = np.array((x, y))
     plt.scatter(x,y)
     plt.show()
     return pt
 
-def smart_sampler(pt, pixel_x, pixel_y, scale, map):
+def smart_sampler(pt, pixel_x, pixel_y, map):
     x = pt[0]
     y = pt[1]
+    z = pt[2]
     for i in range(0, len(x)):
         x[i] = max(x[i], pixel_x / 2)
     counter = 0
     for i in range(0, len(x)):
-        scale_factor = random.uniform(1 - scale, 1 + scale)
-        sample = map[int(y[i] - (pixel_y * scale_factor) / 2):int(y[i] + (pixel_y * scale_factor) / 2), int(x[i] - (pixel_x * scale_factor) / 2): int(x[i] + (pixel_x * scale_factor) / 2)]
-        sample = cv2.resize(sample, (640, 480))
-        if sample.shape == (pixel_y, pixel_x, 3) and cv2.countNonZero(cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)) != 0:
+        scale_factor = 1 + z[i]
+        pixel_scale_x = int(pixel_x * scale_factor)
+        pixel_scale_y = int(pixel_y * scale_factor)
+        sample = map[int(y[i] - (pixel_scale_y) / 2):int(y[i] + (pixel_scale_y) / 2), int(x[i] - (pixel_scale_x) / 2): int(x[i] + (pixel_scale_x) / 2)]
+        if sample.shape == (pixel_scale_y, pixel_scale_x, 3) and cv2.countNonZero(cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)) != 0:
+            sample = cv2.resize(sample, (pixel_x, pixel_y))
             counter += 1
             filename = "sample_folder/sample_number" + str(counter) + ".png"
             gps_coordinates(filename, x[i], y[i])
@@ -102,4 +109,4 @@ if __name__ == "__main__":
     map = cv2.imread("img_save/V2_map_campus/map_campus_NM_MB.png")
     clear_folder('sample_folder/*')
     #x, y = elliptical_path("img_save/V2_map_campus/map_campus_NM_MB.png", 1)
-    smart_sampler(gen_elliptical_path("img_save/V2_map_campus/map_campus_NM_MB.png", 120), 640, 480, 0.8, map)
+    smart_sampler(gen_elliptical_path("img_save/V2_map_campus/map_campus_NM_MB.png", 100), 640, 480, map)
