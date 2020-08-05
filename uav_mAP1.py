51e5490c0b4b38536a0469dafc92eb171af8427e
Alessandro Panini <ge97dil@mytum.de>
2020-08-05 15:25:48 +0200

    Going in both dierction the algorithm works The only problem is taht the blur keep being there since it need to copy it every time again to a new fram applying a H matrix.

--- uav_mAP1.py
+++ uav_mAP1.py
@@ -30,16 +30,15 @@ def load_images_from_folder(folder):
     print("Images loaded")
     return images
 
-def findDimensions(image, H_inv):
+def findDimensions(image, H_inv, H):
     # Initialize 1x3 array of 1 to hold x,y,channel
     base_p1 = np.ones(3, np.float32)
     base_p2 = np.ones(3, np.float32)
     base_p3 = np.ones(3, np.float32)
     base_p4 = np.ones(3, np.float32)
-
     # Get image Height and Width
     (y, x) = image.shape[:2]
-
+    # print(np.linalg.det(H[:2, :2])/math.sqrt(H[0, 0] ** 2 + H[1, 0] ** 2))
     # Store corner points
     base_p1[:2] = [0, 0]
     base_p2[:2] = [x, 0]
@@ -53,11 +52,12 @@ def findDimensions(image, H_inv):
     min_y = None
 
     # For each one of the points we need to apply the inverse Homography matrix in order to transform the points of the next image to the base frame
-    print("H_inv" + str(H_inv))
     for pt in [base_p1, base_p2, base_p3, base_p4]:
-
         # Transform next_image corners into the base space
         hp = np.matmul(np.array(H_inv, np.float32), np.array(pt, np.float32).reshape(-1,1))
+        #print("Array" + str(np.array(pt, np.float32).reshape(-1,1)) + "\n")
+        #print("Matrix" + str(np.array(H_inv, np.float32)) + "\n")
+        #print("Output vector" + str(hp) + "\n")
         # Normalize the coordinates, by defining the correct point the output is a line vector
         normal_pt = np.array([hp[0, 0] / hp[2, 0], hp[1, 0] / hp[2, 0]], np.float32)
         # In order to define a rectangular hull containing the image, keep only min and max values of x,y
@@ -72,6 +72,7 @@ def findDimensions(image, H_inv):
 
         if (min_y == None or normal_pt[1] < min_y):
             min_y = normal_pt[1]
+
     # Limit to zero the value of min_x and min_y
     min_x = min(0, min_x)
     min_y = min(0, min_y)
@@ -115,33 +116,38 @@ def stitching(full_img, next_img, H, vector, offset_value):
     # Inverse matrix
     H_inv = linalg.inv(H)
     # Find the rectangular hull containing the next_img in the base_img reference frame
-    (min_x, min_y, max_x, max_y) = findDimensions(next_img,  H_inv)
+    (min_x, min_y, max_x, max_y) = findDimensions(next_img,  H_inv, H)
     # Adjust max_x, max_y in order to include also the first image
     max_x = max(max_x, full_img.shape[1])
     max_y = max(max_y, full_img.shape[0])
     # Define the move vector for each pixel
     move_h = np.identity(3, np.float64)
     # Translate the origin of the image in the positive part of the plane, in order to see it
-    if (min_x < 0):
+    if min_x < 0 or min_y < 0:
+        move_h = H
+        move_h[0,2] = 0
+        move_h[1,2] = 0
         move_h[0, 2] += -min_x
         max_x += -min_x
-
-    if (min_y < 0):
         move_h[1, 2] += -min_y
         max_y += -min_y
+        img_w = int(math.ceil(max_x) * math.sqrt(H[0, 0] ** 2 + H[1, 0] ** 2))
+        img_h = int(math.ceil(max_y) * np.linalg.det(H[:2, :2]) / math.sqrt(H[0, 0] ** 2 + H[1, 0] ** 2))
+    else:
+        img_w = int(math.ceil(max_x))
+        img_h = int(math.ceil(max_y))
     # First the second image need to be taken to the first image reference frame (H inv) and then need to be
     # translated by move_h. The two transformation can be coupled in mod_inv_h
     mod_inv_h = np.matmul(H_inv, move_h)
     # Return the closest integer near a given number
-    img_w = int(math.ceil(max_x))
-    img_h = int(math.ceil(max_y))
+    print(img_h, img_w)
 
     next_img_center_point = np.identity(3, np.float32)
-    next_img_center_point[0, 2] = (next_img.shape[1]/2) * math.sqrt(H[0,0]**2 + H[1,0]**2)
-    next_img_center_point[1, 2] = (next_img.shape[0]/2) * np.linalg.det(H[:2, :2])/math.sqrt(H[0, 0] ** 2 + H[1, 0] ** 2)
+    next_img_center_point[0, 2] = (next_img.shape[1]/2)
+    next_img_center_point[1, 2] = (next_img.shape[0]/2)
     # print("Next image center point", next_img_center_point,"\n")
     next_img_center_point = np.matmul(H_inv, next_img_center_point)
-    print("Center point X:", next_img_center_point[0,2],"Y:", next_img_center_point[1,2], "\n")
+    # print("Center point X:", next_img_center_point[0,2],"Y:", next_img_center_point[1,2], "\n")
 
     pts = np.array(next_img_center_point)
     vector.append(pts)
@@ -158,7 +164,8 @@ def stitching(full_img, next_img, H, vector, offset_value):
     edge_4 = np.array([[last_img_x_center - int(next_img.shape[1] / 2) - offset_value, last_img_y_center + int(next_img.shape[0] / 2) + offset_value]])
 
     edges = np.array([edge_1, edge_2, edge_3, edge_4])
-    print(move_h)
+    # print(move_h)
+    # print(H)
     full_img_warp = cv2.warpPerspective(full_img, move_h, (img_w, img_h))
     cv2.imshow("full_image_warp", full_img_warp)
     cv2.waitKey(30)
@@ -176,10 +183,6 @@ def stitching(full_img, next_img, H, vector, offset_value):
     mask_1 = np.zeros(final_img.shape, dtype=np.uint8)
     cv2.fillPoly(mask_1, pts=[edges], color=(255, 255, 255))
     maksed_image = cv2.bitwise_and(final_img, mask_1)
-
-    #cv2.imshow("tyr",cv2.resize(maksed_image, (640,480)))
-    #cv2.waitKey(20)
-
 
     return final_img, maksed_image,vector
 
@@ -210,7 +213,7 @@ if __name__ == "__main__":
         kp_next_img, kp_descriptor_next_img = features_detection(img2_GS)
         sel_matches = feature_matching(kp_descriptor_base_img, kp_descriptor_next_img)
         H = find_homography(kp_base_img, kp_next_img, sel_matches)
-        print("Current homography matrix scale introduced on X: " + str(100*(math.sqrt(H[0,0]**2 + H[1,0]**2) - 1)) + " Y:" + str(100 * ((np.linalg.det(H[:2, :2])/math.sqrt(H[0, 0] ** 2 + H[1, 0] ** 2) -1))) + "\n")
+        # print("Current homography matrix scale introduced on X: " + str(100*(math.sqrt(H[0,0]**2 + H[1,0]**2) - 1)) + " Y:" + str(100 * ((np.linalg.det(H[:2, :2])/math.sqrt(H[0, 0] ** 2 + H[1, 0] ** 2) -1))) + "\n")
         if i == 1:
             final_img, neg, next_center = stitching(base_img, next_img, H, vector, offset_value)
         else:
@@ -222,7 +225,7 @@ if __name__ == "__main__":
         with open('csv_plots.csv', 'a', newline = '') as file:
             writer = csv.writer(file)
             writer.writerow([len(kp_base_img), len(kp_next_img), len(sel_matches), (time.time() - start_time)])
-        print("iteration number ", i, " completed")
+        # print("iteration number ", i, " completed")
     final_img = cv2.medianBlur(final_img, 3)
     for i in next_center:
         cv2.circle(final_img, (int(i[0, 2]), int(i[1, 2])), 5,  (255, 255, 0), -1)
@@ -237,7 +240,7 @@ if __name__ == "__main__":
             # print(row_of_interest[i])
             cv2.putText(final_img, "X: " + str(row_of_interest[i][1]) + " Y: " + str(row_of_interest[i][2]), (int(next_center[i][0][2] +10), int(next_center[i][1][2])), cv2.FONT_ITALIC, 0.5 ,(255, 255, 0) )
     cv2.imshow("next", final_img)
-    save_file_name = "img_save/Test1_Pix" + str(images[0].shape[0]) + "_N_img" + str(len(images)) + "_Offset_" + str(offset_value) +".png"
+    save_file_name = "img_save/Linear_sine_zoom_test/REVREV_Test1_Pix" + str(images[0].shape[0]) + "X" + str(images[0].shape[1]) + "_N_img" + str(len(images)) + "_Offset_" + str(offset_value) +".png"
     cv2.imwrite(save_file_name, final_img)
     cv2.waitKey()
 
