8da575a04aff1e04075e050b14ddb90cf9bf2652
Alessandro Panini <ge97dil@mytum.de>
2020-09-04 13:18:02 +0200

    ADD Rotation related to tangent

--- test_path.py
+++ test_path.py
@@ -28,8 +28,8 @@ def gen_sin_path(img_path, sample):
     z = []
     for i in range(1, 2 * sample + 1):
         x.append(300 + 8 * i)
-        y.append((int(map.shape[0] / 2 + (0.6 * map.shape[0] / 2) * math.sin(math.radians(9 * i)))))
-        z.append(0.2 * math.sin(math.radians(20 * i)))
+        y.append((int(map.shape[0] / 2 + (0.6 * map.shape[0] / 2) * math.sin(math.radians(7 * i)))))
+        z.append(0 * math.sin(math.radians(20 * i)))
     pt = np.array((x, y, z))
     return pt
 
@@ -89,21 +89,25 @@ def smart_sampler(pt, pixel_x, pixel_y, max_rot_angle, map):
     for i in range(0, len(x)):
         x[i] = max(x[i], pixel_x / 2)
     counter = 0
-    med_overlapping = overlapping_area(x,y, pixel_x, pixel_y)
+    med_overlapping, fps = overlapping_area(x,y, pixel_x, pixel_y)
     for i in range(0, len(x)):
         # SCALE - define a scale factor for each dimension
         scale_factor = 1 + z[i]
         pixel_scale_x = int(pixel_x * scale_factor)
         pixel_scale_y = int(pixel_y * scale_factor)
-        # ROTATION: Rotate the base image and sample, to rotate a homography matrix is introduced
-        rot_angle = np.random.uniform(0, max_rot_angle)
-        H_rot = np.array([(math.cos(math.radians(rot_angle)), -math.sin(math.radians(rot_angle)), 0), (math.sin(math.radians(rot_angle)), math.cos(math.radians(rot_angle)), 0), (0, 0, 1)], np.float32)
-        map_T = cv2.warpPerspective(map, H_rot, map.shape[:2])
+       # ROTATION BASED ON TANGENT
+        if i+1 != len(x):
+            # radians = math.atan2(y[i + 1] - y[i], x[i + 1] - x[i])
+            radians = math.atan2(x[i + 1] - x[i], y[i + 1] - y[i])
+        else:
+            radians = math.atan2(y[i] - y[i - 1], x[i] - x[i - 1])
+
+        M = cv2.getRotationMatrix2D((x[i], y[i]), math.degrees( - radians), 1)
+        map_T = cv2.warpAffine(map, M, (map.shape[1], map.shape[0]))
 
         sample = map_T[int(y[i] - pixel_scale_y / 2):int(y[i] + pixel_scale_y / 2),
                  int(x[i] - pixel_scale_x / 2): int(x[i] + pixel_scale_x / 2)]
-        if sample.shape == (pixel_scale_y, pixel_scale_x, 3) and cv2.countNonZero(
-                cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)) != 0:
+        if sample.shape == (pixel_scale_y, pixel_scale_x, 3) and cv2.countNonZero(cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)) != 0:
             sample = cv2.resize(sample, (pixel_x, pixel_y))
             counter += 1
             filename = "sample_folder/sample_number" + str(counter) + ".png"
@@ -120,7 +124,7 @@ def smart_sampler(pt, pixel_x, pixel_y, max_rot_angle, map):
     fig = plt.figure()
     # show the 3D rotated projection
     ax2 = fig.add_subplot(111, projection='3d')
-    ax2.text2D(0.05, 0.95, "Medium overlapping percentage = " + str(round(med_overlapping, 2)) + "%", transform=ax2.transAxes)
+    ax2.text2D(0.05, 0.95, "Medium overlapping percentage = " + str(round(med_overlapping, 2)) + "%" + ". Req fps: " + str(round(fps, 2)) + "s", transform=ax2.transAxes)
     ax2.plot_surface(X, Y, Z, rstride=10, cstride=10, facecolors=map / 255, shade=False)
     # =============================
     ax2.plot(x, y, z, c='r', marker='o')
@@ -136,14 +140,18 @@ def gps_coordinates(filename, x, y):
 def overlapping_area(x_c, y_c, pixel_x, pixel_y):
     Rectangle = namedtuple('Rectangle', 'xmin ymin xmax ymax')
     overlapping_perc = 0
+    dc = 0
     for i in range(0, len(x_c)):
         ra = Rectangle(x_c[i-1] - pixel_x/2, y_c[i-1] - pixel_y/2, x_c[i-1] + pixel_x/2, y_c[i-1] + pixel_y/2)
         rb = Rectangle(x_c[i] - pixel_x/2, y_c[i] - pixel_y/2, x_c[i] + pixel_x/2, y_c[i] + pixel_y/2)
         dx = min(ra.xmax, rb.xmax) - max(ra.xmin, rb.xmin)
         dy = min(ra.ymax, rb.ymax) - max(ra.ymin, rb.ymin)
+        dc += math.hypot(x_c[i] - x_c[i - 1], y_c[i] - y_c[i - 1])
         if (dx >= 0) and (dy >= 0):
             overlapping_perc += (dx * dy) * 100 / (pixel_x * pixel_y)
-    return overlapping_perc/len(x_c)
+    speed = 50
+    frame = dc / (len(x_c) * speed)
+    return overlapping_perc/len(x_c), frame
 
 
 if __name__ == "__main__":
@@ -153,4 +161,4 @@ if __name__ == "__main__":
     map = cv2.imread("img_save/V2_map_campus/map_campus_NM_MB.png")
     clear_folder('sample_folder/*')
     # x, y = elliptical_path("img_save/V2_map_campus/map_campus_NM_MB.png", 1)
-    smart_sampler(gen_sin_path("img_save/V2_map_campus/map_campus_NM_MB.png", 20), 640, 480, 5, map)
+    smart_sampler(gen_lin_path("img_save/V2_map_campus/map_campus_NM_MB.png", 10), 640, 480, 5, map)
