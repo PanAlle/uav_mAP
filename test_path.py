bb4c9a908c8cc7126bb1576499a8287414143104
Alessandro Panini <ge97dil@mytum.de>
2020-09-15 11:07:56 +0200

    Folder added

--- test_path.py
+++ test_path.py
@@ -20,6 +20,7 @@ def clear_folder(folder_path):
         os.remove(f)
     print("Folder cleared")
 
+
 # Generate a sine path over the given image
 def gen_sin_path(img_path, sample):
     map = cv2.imread(img_path)
@@ -27,11 +28,12 @@ def gen_sin_path(img_path, sample):
     y = []
     z = []
     for i in range(1, 2 * sample + 1):
-        x.append(300 + map.shape[1]*i / (2 * sample + 1))
+        x.append(300 + map.shape[1] * i / (2 * sample + 1))
         y.append((int(map.shape[0] / 2 + (0.6 * map.shape[0] / 2) * math.sin(math.radians(7 * i)))))
         z.append(0.2 * math.sin(math.radians(20 * i)))
     pt = np.array((x, y, z))
     return pt
+
 
 # Generate a lienar path over the given image
 def gen_lin_path(img_path, sample):
@@ -56,6 +58,7 @@ def gen_lin_path(img_path, sample):
     pt = np.array((x, y, z))
     return pt
 
+
 # Generate an elliptical path over the given image
 def gen_elliptical_path(img_path, sample):
     map = cv2.imread(img_path)
@@ -68,6 +71,7 @@ def gen_elliptical_path(img_path, sample):
         z.append(0.1 * math.sin(math.radians(8 * theta)))
     pt = np.array((x, y, z))
     return pt
+
 
 # Generate a collapsing ellipses ovet the given image, rotation parameter define the grade of collapsing
 def gen_coll_elliptical_path(img_path, sample, rotation):
@@ -82,6 +86,7 @@ def gen_coll_elliptical_path(img_path, sample, rotation):
         y.append((int(lmb * 0.5 * map.shape[0] / 2 * math.sin(math.radians(theta))) + map.shape[0] / 2))
     pt = np.array((x, y))
     return pt
+
 
 # Get points in pixel based coordinates (px, py, z (z is scale factor and is a number going around 1)) from a csv to sample over a given map
 
@@ -100,6 +105,7 @@ def path_from_data(csv_file):
     print("Points vector created")
     return pt
 
+
 # Creates the samples based on a set of given points, sample wanted dimensions and map
 def smart_sampler(pt, pixel_x, pixel_y, map):
     x = pt[0]
@@ -116,35 +122,36 @@ def smart_sampler(pt, pixel_x, pixel_y, map):
         scale_factor = z[i]
         pixel_scale_x = int(pixel_x * scale_factor)
         pixel_scale_y = int(pixel_y * scale_factor)
-       # Compute the tangent considering 2 concurrent points
-        if i+1 != len(x):
-            # radians = math.atan2(y[i + 1] - y[i], x[i + 1] - x[i])
+        # Compute the  angle of the tangent considering 2 concurrent points
+        if i + 1 != len(x):
             radians = math.atan2(x[i + 1] - x[i], y[i + 1] - y[i])
-            # print("Compute tangent")
         else:
+            # Since for the last sample it's not possible to compute the tangent it get assigned the last computable
+            # value
             radians = math.atan2(y[i] - y[i - 1], x[i] - x[i - 1])
-            # print("Compute tangent")
-        M = cv2.getRotationMatrix2D((x[i], y[i]), math.degrees( - radians), 1)
+        # Compute and apply the rotation to the full image in order have sample that follow the rotation of the path
+        M = cv2.getRotationMatrix2D((x[i], y[i]), math.degrees(- radians), 1)
         map_T = cv2.warpAffine(map, M, (map.shape[1], map.shape[0]))
         # print("Map rotated")
         sample = map_T[int(y[i] - pixel_scale_y / 2):int(y[i] + pixel_scale_y / 2),
                  int(x[i] - pixel_scale_x / 2): int(x[i] + pixel_scale_x / 2)]
         # print("Sample" + str(i) + "computed")
-        if sample.shape == (pixel_scale_y, pixel_scale_x, 3) and cv2.countNonZero(cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)) != 0:
+        if sample.shape == (pixel_scale_y, pixel_scale_x, 3) and cv2.countNonZero(
+                cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)) != 0:
             sample = cv2.resize(sample, (pixel_x, pixel_y))
             counter += 1
             filename = "sample_folder/sample_number" + str(counter) + ".png"
             gps_coordinates(filename, x[i], y[i])
             cv2.imwrite(filename, sample)
-    # # Plot the map on the 3d graph
-    # # =============================
+    # Plot the map on the 3d graph
+    # =============================
     # xx, yy = np.meshgrid(np.linspace(0, map.shape[1], map.shape[1]), np.linspace(0, map.shape[0], map.shape[0]))
     # X = xx
     # Y = yy
     # Z = min(z) * np.ones(X.shape) - 2
-    # # create the figure
+    # create the figure
     # fig = plt.figure()
-    # # show the 3D rotated projection
+    # show the 3D rotated projection
     # ax2 = fig.add_subplot(111, projection='3d')
     # ax2.text2D(0.05, 0.95, "Medium overlapping percentage = " + str(round(med_overlapping, 2)) + "%" + ". Req fps: " + str(round(fps, 2)) + "s", transform=ax2.transAxes)
     # # ax2.plot_surface(X, Y, Z, rstride=5, cstride=5, facecolors=map / 255, shade=False)
@@ -159,13 +166,15 @@ def gps_coordinates(filename, x, y):
         writer = csv.writer(file)
         writer.writerow([filename, x, y])
 
+
 def overlapping_area(x_c, y_c, pixel_x, pixel_y):
     Rectangle = namedtuple('Rectangle', 'xmin ymin xmax ymax')
     overlapping_perc = 0
     dc = 0
     for i in range(0, len(x_c)):
-        ra = Rectangle(x_c[i-1] - pixel_x/2, y_c[i-1] - pixel_y/2, x_c[i-1] + pixel_x/2, y_c[i-1] + pixel_y/2)
-        rb = Rectangle(x_c[i] - pixel_x/2, y_c[i] - pixel_y/2, x_c[i] + pixel_x/2, y_c[i] + pixel_y/2)
+        ra = Rectangle(x_c[i - 1] - pixel_x / 2, y_c[i - 1] - pixel_y / 2, x_c[i - 1] + pixel_x / 2,
+                       y_c[i - 1] + pixel_y / 2)
+        rb = Rectangle(x_c[i] - pixel_x / 2, y_c[i] - pixel_y / 2, x_c[i] + pixel_x / 2, y_c[i] + pixel_y / 2)
         dx = min(ra.xmax, rb.xmax) - max(ra.xmin, rb.xmin)
         dy = min(ra.ymax, rb.ymax) - max(ra.ymin, rb.ymin)
         dc += math.hypot(x_c[i] - x_c[i - 1], y_c[i] - y_c[i - 1])
@@ -173,7 +182,7 @@ def overlapping_area(x_c, y_c, pixel_x, pixel_y):
             overlapping_perc += (dx * dy) * 100 / (pixel_x * pixel_y)
     speed = 50
     frame = dc / (len(x_c) * speed)
-    return overlapping_perc/len(x_c), frame
+    return overlapping_perc / len(x_c), frame
 
 
 if __name__ == "__main__":
--- uav_mAP_old_versions/uav_mAP1 _bigframe.py
+++ uav_mAP_old_versions/uav_mAP1 _bigframe.py
@@ -79,15 +79,26 @@ def features_detection(img):
     kp_pt, kp_descriptor = descriptor.detectAndCompute(img, None)
     return kp_pt, kp_descriptor
 
-def features_detection_next(img, mask_1):
+def features_detection_next(img, mask_1, next_img_warp):
     descriptor = cv2.xfeatures2d.SURF_create()
     # Compute keypoint and relative descriptor, None mask applied to the images
-    # ret, mask = cv2.threshold(mask_1, 0, 255, cv2.THRESH_BINARY)
-    mask = cv2.cvtColor(mask_1, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("full_image_warp", cv2.resize(cv2.bitwise_and(img, mask), (640,480)))
+    ret, mask = cv2.threshold(next_img_warp, 0, 255, cv2.THRESH_BINARY)
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    # img = np.zeros(img.shape, np.uint8)
+    # cv2.imshow("full_image_warp", cv2.resize(mask, (640*3,480*3)))
     # cv2.waitKey(30)
-    print(mask.shape)
+    # cv2.imshow("full_image_warp", cv2.resize(cv2.bitwise_and(next_img_warp, mask), (640*3,480*3)))
+    # cv2.waitKey(30)
+    # print(mask.shape)
     kp_pt, kp_descriptor = descriptor.detectAndCompute(img, mask)
+    img = cv2.drawKeypoints(img, kp_pt, None)
+    # cv2.imshow("full_image_warp", cv2.resize(img, (960, 1280)))
+    # cv2.imshow("mask", cv2.resize(mask, (960, 1280)))
+    # cv2.waitKey(30)
+
+    save_file_name = "img_save/Enlarged_frame/Mask_test/Keypoints" + str(mask_1.shape[:2]) + ".png"
+    cv2.imwrite(save_file_name, img)
+
     return kp_pt, kp_descriptor
 
 def feature_matching(base_img_descriptor, next_img_descriptor):
@@ -103,7 +114,7 @@ def feature_matching(base_img_descriptor, next_img_descriptor):
     # Initialize an array to store selceted matches based on Lowe's ratio
     sel_matches = []
     for m, n in matches:
-        if m.distance < 0.7 * n.distance:
+        if m.distance < 0.6 * n.distance:
             sel_matches.append(m)
     return sel_matches
 
@@ -122,13 +133,12 @@ def find_homography(kp_base_img, kp_next_img, sel_matches):
 def stitching(full_img, next_img, H, vector, offset_value):
     # Normalize to maintaining homogeneous coordinate system
     H = H / H[2, 2]
-
-    print(H)
     # Inverse homography, from the next image frame to the base image frame
     H_inv = linalg.inv(H)
     # Find the rectangular hull containing the next_img and the next_image, since a new reference frame is defined then
     # both base and next image need to be translated
     (min_x, min_y, max_x, max_y) = findDimensions(next_img,  H_inv, H)
+    print(max_x, max_y)
     # Adjust max_x, max_y in order to include also the first image
     max_x = max(max_x, full_img.shape[1])
     max_y = max(max_y, full_img.shape[0])
@@ -137,7 +147,6 @@ def stitching(full_img, next_img, H, vector, offset_value):
     # Translate the origin of the image in the positive part of the plane, in order to see it
     if min_x < 0 or min_y < 0:
         move_h = H
-        print("Move H", H)
         move_h[0, 2] = 0
         move_h[1, 2] = 0
         move_h[0, 2] += -min_x
@@ -153,15 +162,15 @@ def stitching(full_img, next_img, H, vector, offset_value):
     # translated by move_h. The two transformation can be coupled in mod_inv_h
     mod_inv_h = np.matmul(H_inv, move_h)
     # Return the closest integer near a given number
-    print(img_h, img_w)
+    # print(img_h, img_w)
 
     next_img_center_point = np.identity(3, np.float32)
     next_img_center_point[0, 2] = (next_img.shape[1]/2)
     next_img_center_point[1, 2] = (next_img.shape[0]/2)
     # print("Next image center point", next_img_center_point,"\n")
     next_img_center_point = np.matmul(H_inv, next_img_center_point)
-    # print("Center point X:", next_img_center_point[0,2],"Y:", next_img_center_point[1,2], "\n")
 
+    # print("Center point X:", next_img_center_point[0,2],"Y:", next_img_center_point[1,2], "\n")
     pts = np.array(next_img_center_point)
     vector.append(pts)
     # Warp the new image given the homograph from the old image
@@ -171,86 +180,130 @@ def stitching(full_img, next_img, H, vector, offset_value):
     last_img_x_center = int(vector[-1][0][2])
     last_img_y_center = int(vector[-1][1][2])
 
+    # print("Center point X:", last_img_x_center,"Y:", last_img_y_center, "\n")
+    # print("Inverse homography:", H_inv, "\n")
+    # print("Move vector:", move_h, "\n")
+    #
+    # print("Center point X:", last_img_x_center, "Y:", last_img_y_center, "\n")
     edge_1 = np.array([[last_img_x_center - int(next_img.shape[1] / 2) - offset_value, last_img_y_center - int(next_img.shape[0] / 2) - offset_value]])
     edge_2 = np.array([[last_img_x_center + int(next_img.shape[1] / 2) + offset_value, last_img_y_center - int(next_img.shape[0] / 2) - offset_value]])
     edge_3 = np.array([[last_img_x_center + int(next_img.shape[1] / 2) + offset_value, last_img_y_center + int(next_img.shape[0] / 2) + offset_value]])
     edge_4 = np.array([[last_img_x_center - int(next_img.shape[1] / 2) - offset_value, last_img_y_center + int(next_img.shape[0] / 2) + offset_value]])
 
     edges = np.array([edge_1, edge_2, edge_3, edge_4])
-    print(move_h)
+    # print(move_h)
     # print(H)
-    cv2.imshow("full_imag", cv2.resize(full_img, (640,480)))
-    cv2.waitKey(30)
-    full_img_warp = cv2.warpPerspective(full_img, move_h, (img_w, img_h))
-    cv2.imshow("full_image_warp", cv2.resize(full_img_warp, (640,480)))
-    cv2.waitKey(30)
-    next_img_warp = cv2.warpPerspective(next_img, mod_inv_h, (img_w, img_h))
+    # full_img_warp = cv2.warpPerspective(full_img, move_h, (img_w, img_h))
+    # next_img_warp = cv2.warpPerspective(next_img, mod_inv_h, (img_w, img_h))
     enlarged_base_img = np.zeros((img_h, img_w, 3), np.uint8)
 
 
     # Create a mask from the warped image for constructing masked composite (insert black
     # base on next image, covering the first one)
-    (ret, data_map) = cv2.threshold(cv2.cvtColor(next_img_warp, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
-    enlarged_full_img = cv2.add(enlarged_base_img, full_img_warp, mask=np.bitwise_not(data_map), dtype=cv2.CV_8U)
-    final_img = cv2.add(enlarged_full_img, next_img_warp, dtype=cv2.CV_8U)
+    # (ret, data_map) = cv2.threshold(cv2.cvtColor(next_img_warp, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+    try:
+        next_img_warp = cv2.warpPerspective(next_img, mod_inv_h, (img_w, img_h))
+        enlarged_base_img = np.zeros((img_h, img_w, 3), np.uint8)
+        (ret, data_map) = cv2.threshold(cv2.cvtColor(next_img_warp, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+        enlarged_full_img = cv2.add(enlarged_base_img, full_img, mask=np.bitwise_not(data_map),dtype=cv2.CV_8U)
+        final_img = cv2.add(enlarged_full_img, next_img_warp, dtype=cv2.CV_8U)
+    except:
+
+        offset = 100
+        img_w += offset
+        img_h += offset
+
+        offset_H = np.identity(3, np.float32)
+        offset_H[0, 2] = offset / 2
+        offset_H[1, 2] = offset / 2
+
+        full_img = cv2.warpPerspective(full_img, offset_H, (img_w, img_h))
+        next_img_warp = cv2.warpPerspective(next_img, offset_H * mod_inv_h, (img_w, img_h))
+        enlarged_base_img = np.zeros((img_h, img_w, 3), np.uint8)
+        (ret, data_map) = cv2.threshold(cv2.cvtColor(next_img_warp, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+        enlarged_full_img = cv2.add(enlarged_base_img, full_img, mask=np.bitwise_not(data_map),dtype=cv2.CV_8U)
+        final_img = cv2.add(enlarged_full_img, next_img_warp, dtype=cv2.CV_8U)
+    cv2.imshow("mask_1", cv2.resize(enlarged_base_img, (640,480)))
+    cv2.waitKey(30)
+    # cv2.imshow("next_img", cv2.resize(full_img, (640,480)))
+    # cv2.waitKey(30)
 
     # Add the warped image with 8bit/pixel (0 - 255)
     mask_1 = np.zeros(final_img.shape, dtype=np.uint8)
     cv2.fillPoly(mask_1, pts=[edges], color=(255, 255, 255))
     maksed_image = cv2.bitwise_and(final_img, mask_1)
 
-    return final_img, maksed_image, vector, mask_1
+    return final_img, next_img_warp, vector, mask_1
 
 
 if __name__ == "__main__":
-    with open('csv_plots.csv', 'w', newline='') as file:
+    with open('../csv_plots.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["SURF_kp full_img", "SURF_kp next_img", "number of good matches"])
     images = load_images_from_folder("sample_folder")
     base_img = images[0]
-    # huge_image = np.zeros((int(0.4 * images[0].shape[0]*len(images)), int(0.4 * images[0].shape[1]*len(images)), 3), np.uint8)
-    # huge_image[int(huge_image.shape[0]/2): int(huge_image.shape[0]/2) + base_img.shape[0], int(huge_image.shape[1]/2): int(huge_image.shape[1]/2) + base_img.shape[1]] = base_img
-    # base_img = huge_image
-    print("First iteration completed")
+    # huge_image = np.zeros((int(0.25 * images[0].shape[0]*len(images)), int(0.25* images[0].shape[1]*len(images)), 3), np.uint8)
+    huge_image = np.zeros((1000,1000, 3), np.uint8)
+    huge_image[int(huge_image.shape[0]/2 - base_img.shape[0]/2): int(huge_image.shape[0]/2 - base_img.shape[0]/2) + base_img.shape[0], int(huge_image.shape[1]/2 - base_img.shape[1]/2): int(huge_image.shape[1]/2 - base_img.shape[1]/2) + base_img.shape[1]] = base_img
+    base_img = huge_image
     base_img_center_point = np.identity(3, np.float32)
-    base_img_center_point[0, 2] = base_img.shape[1] / 2
-    base_img_center_point[1, 2] = base_img.shape[0] / 2
+    base_img_center_point[0, 2] = huge_image.shape[1] / 2
+    base_img_center_point[1, 2] = huge_image.shape[0] / 2
     base_pts = np.array(base_img_center_point)
-    offset_value = 100
+    print(base_pts)
+    offset_value = 0
     vector = [base_pts]
     for i in range(1, len(images)):
         start_time = time.time()
         next_img = images[i]
         if i == 1:
+            edge_1 = np.array([[int(huge_image.shape[1] / 2 - images[0].shape[1] / 2) - offset_value,
+                                int(huge_image.shape[0] / 2 - images[0].shape[0] / 2) - offset_value]])
+            edge_2 = np.array([[int(huge_image.shape[1] / 2 + images[0].shape[1] / 2) + offset_value,
+                                int(huge_image.shape[0] / 2 - images[0].shape[0] / 2) - offset_value]])
+            edge_3 = np.array([[int(huge_image.shape[1] / 2 + images[0].shape[1] / 2) + offset_value,
+                                int(huge_image.shape[0] / 2 + images[0].shape[0] / 2) + offset_value]])
+            edge_4 = np.array([[int(huge_image.shape[1] / 2 - images[0].shape[1] / 2) - offset_value,
+                                int(huge_image.shape[0] / 2 + images[0].shape[0] / 2) + offset_value]])
+
+            edges = np.array([edge_1, edge_2, edge_3, edge_4])
+
+            mask_1 = np.zeros(huge_image.shape, dtype=np.uint8)
+            cv2.fillPoly(mask_1, pts=[edges], color=(255, 255, 255))
+
             img1_GS = cv2.GaussianBlur(cv2.cvtColor(base_img, cv2.COLOR_BGR2GRAY), (5, 5), 0)
             img2_GS = cv2.GaussianBlur(cv2.cvtColor(next_img, cv2.COLOR_BGR2GRAY), (5, 5), 0)
             kp_base_img, kp_descriptor_base_img = features_detection(img1_GS)
             kp_next_img, kp_descriptor_next_img = features_detection(img2_GS)
         else:
-            img1_GS = cv2.GaussianBlur(cv2.cvtColor(neg, cv2.COLOR_BGR2GRAY), (5, 5), 0)
+            img1_GS = cv2.GaussianBlur(cv2.cvtColor(final_img, cv2.COLOR_BGR2GRAY), (5, 5), 0)
             img2_GS = cv2.GaussianBlur(cv2.cvtColor(next_img, cv2.COLOR_BGR2GRAY), (5, 5), 0)
-            kp_base_img, kp_descriptor_base_img = features_detection_next(img1_GS, mask_1)
+            kp_base_img, kp_descriptor_base_img = features_detection(img1_GS)
             kp_next_img, kp_descriptor_next_img = features_detection(img2_GS)
+
         sel_matches = feature_matching(kp_descriptor_base_img, kp_descriptor_next_img)
         H = find_homography(kp_base_img, kp_next_img, sel_matches)
-        # print("Current homography matrix scale introduced on X: " + str(100*(math.sqrt(H[0,0]**2 + H[1,0]**2) - 1)) + " Y:" + str(100 * ((np.linalg.det(H[:2, :2])/math.sqrt(H[0, 0] ** 2 + H[1, 0] ** 2) -1))) + "\n")
+
         if i == 1:
-            final_img, neg, next_center, mask_1 = stitching(base_img, next_img, H, vector, offset_value)
+            final_img, neg, next_center, mask_2 = stitching(base_img, next_img, H, vector, offset_value)
         else:
-            final_img, neg, next_center, mask_1 = stitching(final_img, next_img, H, vector, offset_value)
+            final_img, neg, next_center, mask_2 = stitching(final_img, next_img, H, vector, offset_value)
 
-        # print(next_center[1])
         base_img = final_img
-
-        with open('csv_plots.csv', 'a', newline = '') as file:
+        cv2.imshow("next_img", cv2.resize(final_img, (640,480)))
+        cv2.waitKey(30)
+        with open('../csv_plots.csv', 'a', newline ='') as file:
             writer = csv.writer(file)
             writer.writerow([len(kp_base_img), len(kp_next_img), len(sel_matches), (time.time() - start_time)])
         print("iteration number ", i, " completed")
+
+    # =========== end of cycle ================
+
     final_img = cv2.medianBlur(final_img, 3)
     for i in next_center:
         cv2.circle(final_img, (int(i[0, 2]), int(i[1, 2])), 5,  (255, 255, 0), -1)
     row_of_interest = []
-    with open('gps_xyz.csv', 'r') as file:
+    with open('../gps_xyz.csv', 'r') as file:
         csv_reader = csv.reader(file, delimiter=',')
         header = next(csv_reader)
         for row in csv_reader:
@@ -259,10 +312,10 @@ if __name__ == "__main__":
         if i % 5 == 0:
             # print(row_of_interest[i])
             cv2.putText(final_img, "X: " + str(row_of_interest[i][1]) + " Y: " + str(row_of_interest[i][2]), (int(next_center[i][0][2] +10), int(next_center[i][1][2])), cv2.FONT_ITALIC, 0.5 ,(255, 255, 0) )
-    cv2.imshow("next", final_img)
-    save_file_name = "img_save/Linear_sine_zoom_test/REVREV_Test1_Pix" + str(images[0].shape[0]) + "X" + str(images[0].shape[1]) + "_N_img" + str(len(images)) + "_Offset_" + str(offset_value) +".png"
+    # cv2.imshow("next", final_img)
+    save_file_name = "img_save/Enlarged_frame/Mask_test/Test1_with_mask_Pix" + str(images[0].shape[0]) + "X" + str(images[0].shape[1]) + "_N_img" + str(len(images)) + "_Offset_" + str(offset_value) +".png"
     cv2.imwrite(save_file_name, final_img)
-    cv2.waitKey()
+    # cv2.waitKey()
 
 
 
